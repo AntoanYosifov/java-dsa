c845cb1a12a250bd536d5a20014f0fe919172616
Antoan Yosifov <134812071+AntoanYosifov@users.noreply.github.com>
2026-03-19 09:26:27 +0100

    LengthOfLastWord (easy)

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import easy.LengthOfLastWord;
+
+public class Main {
+    public static void main(String[] args) {
+        LengthOfLastWord solution = new LengthOfLastWord();
+
+        int result = solution.lengthOfLastWord("a ");
+        System.out.println(result);
+    }
+}
