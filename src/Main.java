a6e93066f747410a4a6d89cc87dbb65f642e39e5
Antoan Yosifov <134812071+AntoanYosifov@users.noreply.github.com>
2026-03-26 08:34:14 +0100

    SqrtX (easy)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import easy.AddIntegerStrings;
+import easy.SqrtX;
 
 public class Main {
     public static void main(String[] args) {
-        AddIntegerStrings solution = new AddIntegerStrings();
-        System.out.println(solution.addIntegerStrings("1090", "9011"));
+        SqrtX sqrtX = new SqrtX();
+        System.out.println(sqrtX.mySqrt(35));
     }
 }
